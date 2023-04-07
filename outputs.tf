d695a22f39771be1ebd197b4a7338849fc18865c
Moises Delgado <moises.f.delgado@gmail.com>
2023-04-07 13:39:05 -0400

    terraform code now outputs the lambda function names

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "rendered_policy" {
     value = data.aws_iam_policy_document.policy_data.json
 }
+
+output "lambda_get_function" {
+    value = aws_lambda_function.lambda_function_get.function_name
+}
+
+output "lambda_set_function" {
+    value = aws_lambda_function.lambda_function_set.function_name
+}
