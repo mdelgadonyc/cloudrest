5f36798757c0574282f768fd96dfa8f990c22d80
Moises Delgado <moises.f.delgado@gmail.com>
2023-04-07 15:56:32 -0400

    add API portion to terraform and output base url

--- main.tf
+++ main.tf
@@ -120,3 +120,72 @@ resource "aws_lambda_function" "lambda_function_get" {
         }
     }
 }
+
+# Deploy Serverless Applications with AWS Lambda and API Gateway
+# https://developer.hashicorp.com/terraform/tutorials/aws/lambda-api-gateway
+
+resource "aws_apigatewayv2_api" "api_pyset" {
+    name                = "pyget-lambda-pyget-gw"
+    protocol_type       = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "api_pyset" {
+    api_id = aws_apigatewayv2_api.api_pyset.id
+    
+    name        = "pyget-lambda-stage"
+    auto_deploy = true
+
+    access_log_settings {
+        destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+        format = jsonencode({
+            requestId               = "$context.requestId"
+            sourceIp                = "$context.identity.sourceIp"
+            requestTime             = "$context.requestTime"
+            protocol                = "$context.protocol"
+            httpMethod              = "$context.httpMethod"
+            resourcePath            = "$context.resourcePath"
+            routeKey                = "$context.routeKey"
+            status                  = "$context.status"
+            responseLength          = "$context.responseLength"
+            integrationErrorMessage = "$context.integrationErrorMessage"
+        })
+    }
+}
+
+resource "aws_apigatewayv2_integration" "pyset_integration" {
+    api_id = aws_apigatewayv2_api.api_pyset.id
+
+    integration_uri         = aws_lambda_function.lambda_function_get.invoke_arn
+    integration_type        = "AWS_PROXY"
+    integration_method      = "POST"
+
+}
+
+resource "aws_apigatewayv2_route" "pyset_route" {
+    api_id = aws_apigatewayv2_api.api_pyset.id
+
+    route_key = "GET /pet"
+    target    = "integrations/${aws_apigatewayv2_integration.pyset_integration.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+    name = "/aws/api-gw/${aws_apigatewayv2_api.api_pyset.name}"
+
+    retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+    statement_id    = "AllowExecutionFromAPIGateway"
+    action          = "lambda:InvokeFunction"
+    function_name   = aws_lambda_function.lambda_function_set.function_name
+    principal       = "apigateway.amazonaws.com"
+
+    source_arn      = "${aws_apigatewayv2_api.api_pyset.execution_arn}/*/*"
+}
+
+# possible API Gateway permission issue
+# https://repost.aws/knowledge-center/api-gateway-lambda-stage-variable-500
+#
+# $ curl https://mbxwzgfvd6.execute-api.us-east-1.amazonaws.com/pyget-lambda-stage/pet
+# {"message":"Internal Server Error"}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,9 @@ output "lambda_get_function" {
 output "lambda_set_function" {
     value = aws_lambda_function.lambda_function_set.function_name
 }
+
+output "api_get_base_url" {
+    description = "Base URL for get API Gateway Stage"
+
+    value       = aws_apigatewayv2_stage.api_pyset.invoke_url
+}
