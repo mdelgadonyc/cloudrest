c6e4d62f520616a33c865eb5ab84fe7f7b34da73
Moises Delgado <moises.f.delgado@gmail.com>
2023-04-12 16:37:21 -0400

    Test code sets then retrieves record with curl

--- tests/api-set-test.py
+++ tests/api-set-test.py
@@ -12,9 +12,13 @@ urlRegex = re.compile(r'https://.*stage')
 # ie. https://hostname.execute-api.region.amazonaws.com/function-stage
 url = urlRegex.search(cmd_output.decode()).group()
 
-cmd1 = f"curl {url}/pet?id=501"
+cmd1 = f'curl -X POST {url}/pet -H \'Content-Type: application/json\' -d \'{{ "id": "1234", "name": "biscuit", "breed": "pug", "gender": "female", "owner": "allen ginsberg", "birthday": "10/2/2020" }}\''
+
 cmd_output = subprocess.check_output(cmd1, shell = True)
+recSetRegex = re.compile(r'Record 1234 added')
+print (recSetRegex.search(cmd_output.decode()).group())
 
-# {"message":"Not Found"}
-print (cmd_output.decode())
-
+cmd1 = f"curl {url}/pet?id=1234"
+cmd_output = subprocess.check_output(cmd1, shell = True)
+recGetRegex = re.compile(r'ginsberg')
+print (recGetRegex.search(cmd_output.decode()).group())
